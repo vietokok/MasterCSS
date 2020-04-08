a8602b382ae587ef0e34c5a2b874fcc45259e469
vietokok <khacviet201299@gmail.com>
2020-04-08 17:09:18 +0700

    Add 3-404 Error Page

---
+++ 3-404 Error Page/script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+	let body = document.body;
+	const arrayColor = [
+		"#ffc370",
+		"#fff5f2",
+		"#00ffec",
+		"#ffcc6f",
+		"#49d6ff",
+		"#c8d5ff",
+	];
+	setInterval(() => {
+		createStar();
+	}, 50);
+
+	const createStar = () => {
+		let color = Math.floor(Math.random() * 6);
+		let top = Math.floor(Math.random() * screen.height);
+		let right = Math.floor(Math.random() * 500);
+		let star = document.createElement("div");
+		star.classList.add("star");
+		star.style.right = right + "px";
+		star.style.top = top + "px";
+		star.style.background = arrayColor[color];
+		body.appendChild(star);
+
+		const loopRun = setInterval(() => {
+			if (right >= screen.width) {
+				star.remove();
+				clearInterval(loopRun);
+			}
+			runStar();
+		}, 10);
+
+		const runStar = () => {
+			right += 5;
+			star.style.right = right + "px";
+		};
+		setTimeout(() => {
+			star.remove();
+		}, 6000);
+	};
+});
